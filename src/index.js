067de1b84adcd2b28a2fd194d50a11a890510d32
kittyxcode <katherine.pino059@gmail.com>
2023-08-23 15:07:56 -0400

    Add event and dom manipulation en analizer.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+//obtener elemento dom a observar
+const textarea = document.querySelector("textarea");
+
+//agregar listener y funcion asociada
+textarea.addEventListener("input", updateValuesController);
+
+//En esta funcion se actualiza
+function updateValuesController() {
+  const numero = analyzer.getCharacterCount(textarea.value);
+  alert(numero);
+}
