da86ce5dbf501aa5fe9b1fd06002817076cde9c5
kittyxcode <katherine.pino059@gmail.com>
2023-08-31 15:55:07 -0400

    Completar contar numeros

--- src/analyzer.js
+++ src/analyzer.js
@@ -15,7 +15,7 @@ const analyzer = {
     //recorrer el texto letra por letra y si es letra sumarla a letrasTotales
     for (let index = 0; index < text.length; index++) {
       //si es letra agregar uno al contador de letras
-      if (text[index] !== " " && text[index] !== "," && text[index]!==".") {
+      if (text[index] !== " " && text[index] !== "," && text[index] !== ".") {
         letrasTotales++;
       }
     }
@@ -25,17 +25,21 @@ const analyzer = {
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
     const arrayPalabras = text.trim().split(" ");
-    let suma=0;
-    let promedio=0;
-    arrayPalabras.forEach(palabra => {
-      suma+=palabra.length;
+    let suma = 0;
+    let promedio = 0;
+    arrayPalabras.forEach((palabra) => {
+      suma += palabra.length;
     });
-    promedio = suma/arrayPalabras.length;
+    promedio = suma / arrayPalabras.length;
     return parseFloat(promedio.toFixed(2));
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    text.length;
+    const numerosEncontrados = text.match(/(\d+\.\d+|\d+)(?![\w\d])/g);
+    if (numerosEncontrados === null) {
+      return 0;
+    }
+    return numerosEncontrados.length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
