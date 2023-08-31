bb039786e0edb7664b0b9971d92e9d67936da696
kittyxcode <katherine.pino059@gmail.com>
2023-08-31 16:27:11 -0400

    Completar sumar numeros

--- src/analyzer.js
+++ src/analyzer.js
@@ -10,11 +10,8 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    //crear variable que guarde total de letras
     let letrasTotales = 0;
-    //recorrer el texto letra por letra y si es letra sumarla a letrasTotales
     for (let index = 0; index < text.length; index++) {
-      //si es letra agregar uno al contador de letras
       if (text[index] !== " " && text[index] !== "," && text[index] !== ".") {
         letrasTotales++;
       }
@@ -43,7 +40,16 @@ const analyzer = {
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    text.length;
+    const numerosEncontrados = text.match(/(\d+\.\d+|\d+)(?![\w\d])/g);
+    let sum = 0;
+    if (numerosEncontrados === null) {
+      return 0;
+    } else {
+      numerosEncontrados.forEach((number) => {
+        sum += parseFloat(number);
+      });
+    }
+    return sum;
   },
 };
 
