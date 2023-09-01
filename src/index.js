6d5264d8ca5e364a6a142348d9b4710ff1ff869c
kittyxcode <katherine.pino059@gmail.com>
2023-09-01 14:09:45 -0400

    Update Styles

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import analyzer from "./analyzer.js";
 const textarea = document.querySelector('textarea[name="user-input"]');
 //obtener lista de elementos li
 const listLI = document.getElementsByClassName("li-style");
+const button= document.getElementById("reset-button");
 
 //agregar listener y funcion asociada
 textarea.addEventListener("input", updateValuesController);
+textarea.addEventListener("keyup", updateValuesController)
+button.addEventListener("click", clearControl);
 
 //En esta funcion se actualiza los valores de texto de los 6 li
 function updateValuesController() {
@@ -28,8 +31,18 @@ function updateValuesController() {
   listLI[3].textContent = "Numeros: " + numbersCount;
 
   const numbersSum = analyzer.getNumberSum(textarea.value);
-  listLI[4].textContent = "Suma Numeros: " + numbersSum;
+  listLI[4].textContent = "Suma números:" + numbersSum;
 
   const averageWordLength = analyzer.getAverageWordLength(textarea.value);
   listLI[5].textContent = "Promedio longitud: " + averageWordLength;
+}
+
+function clearControl(){
+  listLI[0].textContent = "Caracteres: 0";
+  listLI[1].textContent = "Caracteres sin espacio: 0";
+  listLI[2].textContent = "Palabras: 0";
+  listLI[3].textContent = "Numeros: 0";
+  listLI[4].textContent = "Suma números: 0";
+  listLI[5].textContent = "Promedio longitud: 0"
+  textarea.value="";
 }
